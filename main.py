e2974b8552c668346b9d718ee5faa0afd6be20b5
Ding Ziheng <1256439121@qq.com>
2022-09-08 19:38:48 +0800

    主功能完成

--- main.py
+++ main.py
@@ -6,22 +6,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--folder_path', type=str, help='folder that contains lots of ._* files')
 
 
-def RemoveDotFile(folder_path):
-    # 找到._开头的文件
-    if not os.path.isdir(folder_path):
-        print('文件夹路径错误')
-        return
+def RemoveDotFile(files):
 
-    file_list = [file for file in os.listdir(folder_path) if file.startswith('._')]
-
-    if len(file_list) == 0:
+    if len(files) == 0:
         print('找不到._开头的文件')
         return
 
-    print('找到以下文件：')
-    for file in file_list:
-        print(file)
-    print('共', len(file_list), '个')
+    print('共', len(files), '个')
     print('是否删除(y/n)？', end='')
     while True:
         ch = input()
@@ -30,18 +21,46 @@ def RemoveDotFile(folder_path):
 
     print('')
     if ch == 'y':
-        for file in file_list:
-            file_path = os.path.join(folder_path, file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            elif os.path.isdir(file_path):
-                os.rmdir(file_path)
+        for file in files:
+            if os.path.isfile(file):
+                os.remove(file)
+            elif os.path.isdir(file):
+                os.rmdir(file)
         print('已全部删除')
     elif ch == 'n':
         print('已取消')
 
 
+def TraverseDir(folder_path):
+    q = list()
+    files = list()
+
+    if not os.path.isdir(folder_path):
+        print('文件夹路径错误')
+        return
+
+    # 遍历所有文件
+    q.append(folder_path)
+    while len(q):
+        folder = q.pop(0)
+
+        # 遍历文件夹下的所有文件
+        for file in os.listdir(folder):
+            full_path = os.path.join(folder, file)
+
+            # ._开头的文件加入列表中
+            if file.startswith('._'):
+                files.append(full_path)
+                print(full_path)
+
+            # 文件夹纳入待搜索队列
+            elif os.path.isdir(full_path):
+                q.append(full_path)
+
+    RemoveDotFile(files)
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     folder_path = args.folder_path
-    RemoveDotFile(folder_path)
+    TraverseDir(folder_path)
